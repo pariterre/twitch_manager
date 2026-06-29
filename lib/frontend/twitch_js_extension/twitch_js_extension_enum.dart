f5a70f995a794b3f6668e220187199634aef30fd
Pariterre <pariterre@hotmail.com>
2026-06-29 18:05:36 -0400

    Fixed name of TwitchAnchor

--- lib/frontend/twitch_js_extension/twitch_js_extension_enum.dart
+++ lib/frontend/twitch_js_extension/twitch_js_extension_enum.dart
@@ -2,18 +2,55 @@ enum TwitchAnchor {
   panel,
   overlay,
   component,
-  unknown,
+  unknown;
+
+  String get name {
+    switch (this) {
+      case TwitchAnchor.panel:
+        return 'panel';
+      case TwitchAnchor.overlay:
+        return 'video_overlay';
+      case TwitchAnchor.component:
+        return 'component';
+      case TwitchAnchor.unknown:
+        return 'unknown';
+    }
+  }
 }
 
 enum TwitchPlatform {
   web,
   mobile,
-  unknown,
+  unknown;
+
+  String get name {
+    switch (this) {
+      case TwitchPlatform.web:
+        return 'web';
+      case TwitchPlatform.mobile:
+        return 'mobile';
+      case TwitchPlatform.unknown:
+        return 'unknown';
+    }
+  }
 }
 
 enum TwitchMode {
   viewer,
   dashboard,
   config,
-  unknown,
+  unknown;
+
+  String get name {
+    switch (this) {
+      case TwitchMode.viewer:
+        return 'viewer';
+      case TwitchMode.dashboard:
+        return 'dashboard';
+      case TwitchMode.config:
+        return 'config';
+      case TwitchMode.unknown:
+        return 'unknown';
+    }
+  }
 }
